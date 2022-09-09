6d7681209a71ac8f5472c48c84bd8db3dc12a606
Favour04 <idaeworfavour1@gmail.com>
2022-09-08 20:50:50 -0700

    Write a script that runs a C file through the preprocessor and save the result into another file.

---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+# 942 "/usr/include/stdio.h" 3 4
+
+
+
+# 2 "main.c" 2
+
+
+
+
+
+# 3 "main.c"
+int main(void)
+{
+	return (0);
+
+}
