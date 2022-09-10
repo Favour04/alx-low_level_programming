cd97b68ddce0f4bed7d20e1eff937188150e2a34
Favour04 <idaeworfavour1@gmail.com>
2022-09-10 04:42:09 -0700

    write a c programme to print a string of words

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,9 +5,11 @@
 *
 * Result: Always 0 (Success)
 */
-int main()
+int main(int argc, char *argv[]);
 {
-puts("\"Programming is like building a multilingual puzzle\n");
+char str[] = "\"Programming is like building a multilingual puzzle\n";
+
+puts(str);
 return (0);
 
 }
