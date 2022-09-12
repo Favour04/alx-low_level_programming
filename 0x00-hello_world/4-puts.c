6bc8f49335ab75aad063c722f521b3e5dd58e40c
Favour04 <idaeworfavour1@gmail.com>
2022-09-12 13:23:29 -0700

    write a c program that will print a string using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
 /**
-* main - Entry point
-*
-* Result: Always 0 (Success)
+ * main - entry point
+ *
+ * print string with puts function
+ * Return: 0 (Success)
 */
-int main(int argc, char *argv[]);
+int main(void)
 {
-char str[] = "\"Programming is like building a multilingual puzzle\n";
+	char *c = "\"Programming is like building a multilingual puzzle";
 
-puts(str);
-return (0);
+	puts(c);
+
+	return (0);
 
 }
