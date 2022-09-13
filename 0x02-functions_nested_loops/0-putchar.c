cd0b25c6a34e92b48def651dae9a34cdd37f57ee
Favour04 <idaeworfavour1@gmail.com>
2022-09-13 15:31:11 -0700

    Write a c code and push it uncompied

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * _putchar - Entry point
+ * 
+ *@char: print _putchar
+ *
+ * Return: 0 (Success)
+*/
+int _putchar(char c);
+{ 
+	char a[] = "_putchar";
+	int b;
+
+	for (b = 0; b < 8; b++)
+	{
+		_putchar(a[b])
+	}
+
+	_putchar ('\n')
+
+	return (0);
+
+}
+
+
+
+
+
+
+
+
+
