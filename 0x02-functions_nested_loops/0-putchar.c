8813dbc985141a3948d4e6d35158f0e0d5df137b
Favour04 <idaeworfavour1@gmail.com>
2022-09-13 15:55:25 -0700

    modify

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,20 +2,21 @@
 /**
  * _putchar - Entry point
  *
- *@char: print _putchar
+ *print: print _putchar
  *
  * Return: 0 (Success)
 */
 int main(void);
 {
-	char a[] = "_putchar";
-	int b;
-
-	for (b = 0; b < 8; b++)
-	{
-		_putchar(a[b])
-	}
-		_putchar ('\n')
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 
