7bc255fcc19b4b01997fc27316066a0ee4bf8e9e
Favour04 <idaeworfavour1@gmail.com>
2022-09-13 16:18:20 -0700

    print alphabet in small letter

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* Alphabets - Entry point
+*
+* Description - print letters in lower case
+* Return: 0 (Success)
+*/
+void print_alphabet(void);
+{
+	int c;
+
+	for (c = 61; c < 123; c++)
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
