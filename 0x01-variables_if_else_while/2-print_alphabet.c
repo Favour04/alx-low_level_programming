955a399a2387eec53cc136aa52c1d48975881214
Favour04 <idaeworfavour1@gmail.com>
2022-09-12 20:43:37 -0700

    Write a c program to print alphabet in lower and higher case

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,37 +1,22 @@
 #include <stdio.h>
 
-
-
 /**
-
 * main - prints alphabets
-
 *in lower case
-
 * Return: int 0
-
 */
-
-
-
 int main(void)
 
 {
+	int letter;
 
-		int letter;
+	for (letter = '97'; letter < '123'; letter++)
 
+	{
+		putchar(letter);
+	}
+		putchar(10);
 
-
-			for (letter = 97; letter < 123; letter++)
-
-					{
-
-								putchar(letter);
-
-									}
-
-										putchar(10);
-
-											return (0);
+	return (0);
 
 }
