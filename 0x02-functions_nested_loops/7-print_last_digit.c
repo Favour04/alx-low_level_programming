a5ce37ac520e26598b2d49b772596270accfb954
Favour04 <idaeworfavour1@gmail.com>
2022-09-14 15:56:12 -0700

    Write a c program to print the last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Code to get the last digit of a number n
+ * @n: n is the main number
+ * Return: 0 (Success)
+*/
+int print_last_digit(int n)
+{
+	int i;
+
+	for (i = n % 10) 
+	{
+		return (n);
+		_putchar(n);
+	}
+	return (0);
+}
