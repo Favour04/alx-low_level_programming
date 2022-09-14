9892c04fa170afdb7aca4f68aea2e53cfcf1cafe
Favour04 <idaeworfavour1@gmail.com>
2022-09-14 15:42:43 -0700

    Write a c code to show the absolute value of an int

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - print the absolute value of an integer
+ * @n: var
+ * Return: Absolute value
+*/
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+
+	else if (n < 0)
+	{
+		return (-n);
+	}
+		return (0);
+}
