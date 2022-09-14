4d945691cc4af03913674770767898d21b297338
Favour04 <idaeworfavour1@gmail.com>
2022-09-14 07:21:09 -0700

    Print alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,17 +10,16 @@ void print_alphabet_x10(void)
 {
 	int i = 0;
 	int c;
-	
+
 	while (i < 10)
 	{
+		i++;
+	}
 
-		for (c = 97; c < 123; c++)
+		for start[i](c = 97; c < 123; c++)
 		{
 			_putchar(c);
 			c++;
 			_putchar('\n');
 		}
-	}
-
-
 }
