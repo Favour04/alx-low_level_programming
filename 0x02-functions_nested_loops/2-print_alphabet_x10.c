d01bd6586954eb3195238cda7c568c6a28a1449f
Favour04 <idaeworfavour1@gmail.com>
2022-09-13 17:16:49 -0700

    print alphabet in small letter

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * Print ten  timees lower case
+ *
+ * Return: 0 (Success)
+*/
+void print_alphabet_x10(void);
+{
+	int i;
+	int j;
+	while (i =0; i < 9; i++)
+	{
+
+	}
+}
