4130e49f25be235dac9872ac22b8c1a1a62dd396
Favour04 <idaeworfavour1@gmail.com>
2022-09-13 19:15:30 -0700

    to commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,17 +6,15 @@
  * description - print alphabets in lower case
  * Return: Always 0 (Success)
  */
-void print_alphabet(void)
+void print_alphabet(void);
 {
-	int c;
+int c;
 
-	for (c = 'a'; c < 'z'; c++)
-	{
-		_putchar(c);
-	
-	}
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
 
-	_putchar('\n');
-
-	return ;
+return;
 }
