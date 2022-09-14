7164dec9ec9044948116b33e3610c218fe9219a9
Favour04 <idaeworfavour1@gmail.com>
2022-09-14 11:13:51 -0700

    Write a c programme to check string case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * _islower - check numbers casing
+ *
+ * @c: var name
+ * Return: Always 0 success
+*/
+int _islower(int c)
+{
+	/* declare local variable */
+
+	if (islower(c = 'H')
+	{
+		putchar(1);
+	}
+	else
+	{
+		putchar(0);
+	}
+
+	if (islower(c = 'o')
+	{
+		putchar(1);
+	}
+	else
+	{
+		putchar(0);
+	}
+
+	if (islower(c = '108')
+	{
+		putchar(1);
+	}
+	else
+	{
+		putchar(0);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
