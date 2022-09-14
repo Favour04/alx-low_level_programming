0436016113f2c28acf7a382fb204b1f93d9afd7c
Favour04 <idaeworfavour1@gmail.com>
2022-09-14 14:55:51 -0700

    Write a c program that print the signs of number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _islower - check numbers casing
+ *
+ * @c: var name
+ * Return: Always 0 success
+*/
+int print_sign(int n)
+{
+	/* declare local variable */
+
+	if (n >= 48 && n <= 57)
+	{
+		return (1);
+
+	}
+
+	else
+	{
+		return (0);
+
+	}
+
+	_putchar('\n');
+
+}
