9b06e6dfea37cc20870ce26e2ded3ba68a03c2cc
Favour04 <idaeworfavour1@gmail.com>
2022-09-14 14:42:11 -0700

    Write a c program to chech only alphabets

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _isalpha - check numbers casing
+ *
+ * @c: var name
+ * Return: Always 0 success
+*/
+int _isalpha(int c)
+{
+	/* declare local variable */
+
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+
+	}
+
+	else
+	{
+		return (0);
+
+	}
+
+	_putchar('\n');
+
+}
