9fae1db4b9d70458947523c812f1566a38adc0d8
Favour04 <idaeworfavour1@gmail.com>
2022-09-14 19:17:48 -0700

    Auto commit

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio>
+
+/**
+ * print_to_98 - print num
+ * @n: intiger
+ */
+void print_to_98(int n)
+{
+	while (n < 98)
+	{
+		printf("%d, ". n);
+		n++;
+	}
+	while (n == 0)
+	{
+		printff("%d, ", n)
+	}
+
+	if (n == 98)
+		printf("%d, ", n)
+
+		printf("\n");
+}
