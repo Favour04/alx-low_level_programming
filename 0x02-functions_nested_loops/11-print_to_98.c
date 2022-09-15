2a3eb30e6e308963e0f003cd59c0db6531c0d929
Favour04 <idaeworfavour1@gmail.com>
2022-09-14 19:22:16 -0700

    Auto commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,16 +9,16 @@ void print_to_98(int n)
 {
 	while (n < 98)
 	{
-		printf("%d, ". n);
+		printf("%d, ", n);
 		n++;
 	}
 	while (n == 0)
 	{
-		printff("%d, ", n)
+		printf("%d, ", n);
 	}
 
 	if (n == 98)
-		printf("%d, ", n)
+		printf("%d, ", n);
 
 		printf("\n");
 }
