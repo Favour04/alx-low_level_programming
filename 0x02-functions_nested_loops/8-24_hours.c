7c5607e94847e0b60e613031b7e98123ca974ea1
Favour04 <idaeworfavour1@gmail.com>
2022-09-14 17:11:44 -0700

    Write c code to print every minute of a day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * jack_bauer - code to display all seconds of a sound trrack.
+ *
+ * Description: print every minute of a day
+ * Return: 0 (Success)
+ */
+void jack_bauer(void)
+{
+int hours = 0;
+int min = 0;
+
+while (hours < 24)
+{
+while (min < 60)
+{
+_putchar('0' + (hours / 10));
+_putchar('0' + (hours % 10));
+_putchar(':');
+_putchar('0' + (min / 10));
+_putchar('0' + (min % 10));
+_putchar('\n');
+}
+min = 0;
+hours++
+}
+}
