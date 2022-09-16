aef51872d855e5a1ead6b37fec93f1ec36847b49
Favour04 <idaeworfavour1@gmail.com>
2022-09-15 19:18:43 -0700

    New c file

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - Code to check uper case number
+ *@c: variable or character to be checked
+ * Return: 1 if char is upper ; 0 if char is lower
+*/
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	
+		return (1);
+	
+	return (0);
+	
+}
