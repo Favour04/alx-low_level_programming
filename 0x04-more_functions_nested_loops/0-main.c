36c99d565812b3d46b7d8d7411fc27a9361397bd
Favour04 <idaeworfavour1@gmail.com>
2022-09-15 19:20:00 -0700

    add all files

---
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int c;
+	c = 'A';
+
+	_isupper(c);
+
+	return (0);
+}
