ca01ac64a6ad9d2a888a5d8e2d8131429ed181d9
Favour04 <idaeworfavour1@gmail.com>
2022-09-15 22:05:41 -0700

    Write a c program that from 0 to 9, followed by a new line.

---
+++ 0x04-more_functions_nested_loops/4-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i;
+	i = 48;
+
+	print_most_numbers();
+	_putchar(i);
+
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* print_most_numbers - prints numbers
+*
+*/
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
