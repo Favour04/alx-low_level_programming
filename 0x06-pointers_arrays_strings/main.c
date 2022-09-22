0c86e7a6470211c58b068d0a671794911b6c6cb3
Favour04 <idaeworfavour1@gmail.com>
2022-09-22 15:00:53 -0700

    Write a function that capitalizes all words of a string.

---
+++ 0x06-pointers_arrays_strings/main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+*/
+
+int main(void)
+{
+	char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
