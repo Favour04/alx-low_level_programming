5e1ffe9ec55ce920691acb651567a0b58d9845c7
Favour04 <idaeworfavour1@gmail.com>
2022-09-30 21:41:39 +0000

    multiply integers aurgument passed in command line

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,10 +11,10 @@ int main(int argc, char *argv[])
 {
 	int mul;
 
-	if (argc > 1)
+	if (argc == 3)
 	{
-			mul = atoi(argv[1]) * atoi(argv[2]);
-			printf("%d\n", mul);
+		mul = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mul);
 	}
 
 	return (0);
