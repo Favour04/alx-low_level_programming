571882c0531458f8beafd46559fed5c0009ca688
Favour04 <idaeworfavour1@gmail.com>
2022-10-11 22:27:34 -0700

    struct for dog info

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
-typedef struct dog {
+/**
+ * struct dog - dog info
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+typedef struct dog
+{
 	char *name;
 	float age;
 	char *owner;
