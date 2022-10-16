f03a1d95a1139b4aaf00d486829c909c81d97e01
Favour04 <idaeworfavour1@gmail.com>
2022-10-16 03:23:45 +0000

    memset to fill memory with constant bytes

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,17 +3,15 @@
 /**
  * *_memset - function to return pointer to *s
  * @s: string value
- * @b:
- * @n:
+ * @b: char to copy memory
+ * @n: the num of bytes
  * Return: 0
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-
 	for (; n > 0; n--)
 	{
 		s[n - 1] = b;
-		
 	}
 
 	return (s);
