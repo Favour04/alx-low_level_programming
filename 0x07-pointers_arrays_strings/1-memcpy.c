2cafe42df86c51e0cb28bcadf173daa792d9074c
Favour04 <idaeworfavour1@gmail.com>
2022-10-17 01:39:25 +0000

    memcpy function to copy bytes from mem area src to dest

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -3,15 +3,15 @@
 
 /**
  *  *_memcpy - copy n bytes of memory from *src to dest
- * @*src: string to be printed
- * @*dest: pointer to src
+ * @src: string to be printed
+ * @dest: pointer to src
  * @n: num of bytes
  * Return: pointer to dest var
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
-{	
+{
 	while (n > 0)
-	{	
+	{
 		dest[n - 1] = src[n - 1];
 		n--;
 	}
