1d0393d9aef5aecdebb935a7a92ed9a6f3238f6e
Favour04 <idaeworfavour1@gmail.com>
2022-11-14 16:36:05 +0100

    binary_to_unit function to convert binary to uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,7 +5,7 @@
  * @b: binary to convert
  * Return: unit
  */
-unsigned int binary_to_unit(const char *b)
+unsigned int binary_to_uint(const char *b)
 {
 	unsigned int i;
 	unsigned int value;
@@ -15,6 +15,11 @@ unsigned int binary_to_unit(const char *b)
 
 	s = b;
 	/*100*/
+
+	if (b == NULL)
+	{
+		return (0);
+	}
 
 	while (*b != '\0')
 	{
