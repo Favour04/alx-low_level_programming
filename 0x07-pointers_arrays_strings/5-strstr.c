2cf9f88b05ae1bd374bfa1c7c4a255a944a4c9d8
Favour04 <idaeworfavour1@gmail.com>
2023-03-14 13:09:29 +0100

    errors rectified

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,21 +11,36 @@
 
 char *_strstr(char *haystack, char *needle)
 {
+	char *a;
+	int i;
+	int n = _strlen(needle);
+
 	if (haystack == NULL)
 	{
 		return (NULL);
 	}
 
+	a = needle;
+
 	while (*haystack != '\0')
 	{
 		char *s;
 
+		needle = a;
+
 		while (*haystack == *needle)
 		{
-			s = haystack;
+			if (*haystack == *a)
+			{
+				s = haystack;
+			}
 			haystack++;
 			needle++;
-			return (s);
+			i++;
+			if (i == n)
+			{
+				return (s);
+			}
 		}
 		haystack++;
 	}
