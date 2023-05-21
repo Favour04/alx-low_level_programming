ea4bdc14705da204b169f5458ad1dee01319d96b
Favour04 <idaeworfavour1@gmail.com>
2023-05-21 20:47:13 +0100

    done

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,38 @@
 #include "lists.h"
+/**
+ * negative - add length of list with negative number
+ * @cur: list
+ * @i: negative number
+ * Return: sum
+ */
+int negative(dlistint_t *cur, unsigned int i)
+{
+	int j;
 
+	j = 0;
+
+	while (cur != NULL)
+	{
+		cur = cur->next;
+		j++;
+	}
+	i += j;
+	return (i);
+}
+
+/**
+ * zero - hadle first function when the value of idx is 0
+ * @temp: pointer to head
+ * @n: data
+ * Return: pointer to new node
+ */
+dlistint_t *zero(dlistint_t *temp, int n)
+{
+	dlistint_t *new;
+
+	new = add_dnodeint(&temp, n);
+	return (new);
+}
 /**
  * insert_dnodeint_at_index - function to add node to a specified index
  * @h: pointer to head of node
@@ -7,17 +40,21 @@
  * @n: int data
  * Return: pointer to the specified node
  */
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int i;
-	dlistint_t *current;
-	dlistint_t *cur;
-	dlistint_t *new;
-	dlistint_t *pptr;
+	unsigned int i = 0;
+	dlistint_t *current = *h, *cur, *new;
 
-	current = *h;
-	i = 0;
+	if ((int) idx < 0)
+	{
+		idx = negative(*h, idx);
+	}
+
+	if ((int) idx == 0)
+	{
+		new = zero(*h, n);
+		*h = new;
+	}
 
 	while (current != NULL)
 	{
@@ -25,12 +62,17 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		{
 			cur = current;
 		}
+
 		if (i == (idx + 1))
 		{
-			pptr = current;
-			new = add_dnodeint(&pptr, n);
+			new = add_dnodeint(&current, n);
 			new->prev = cur;
 			cur->next = new;
+			return (new);
+		}
+		else if (current->next == NULL)
+		{
+			new = add_dnodeint_end(&current, n);
 			return (new);
 		}
 		current = current->next;
