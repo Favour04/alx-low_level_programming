db45c9708f5f5a35695af77ed080c930b01c5b41
Favour04 <idaeworfavour1@gmail.com>
2023-05-22 08:29:38 +0100

    done

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -63,7 +63,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			cur = current;
 		}
 
-		if (i == (idx + 1))
+		if (i == (idx == 1))
 		{
 			new = add_dnodeint(&current, n);
 			new->prev = cur;
