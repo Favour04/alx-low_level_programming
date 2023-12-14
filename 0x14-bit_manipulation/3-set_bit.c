96ff417bab195ce00690318caabd556238fe4721
FavourId <daeworfavour1@gmail.com>
2023-12-14 21:18:53 +0100

    bit manipulation is cool

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * set_bit: set bit to one at a given index
+ * @index: the index, starting from 0 of the bit you want to set
+ * @n: poiter to the number
+ *
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (!*n)
+		return -1;
+
+	*n = *n | (1 << index);
+
+	return 1;
+		
+}
