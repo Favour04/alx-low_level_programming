fe0e4124d315aab5e61cf03ba1002921fc994804
FavourId <daeworfavour1@gmail.com>
2023-12-15 00:58:54 +0100

    done

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -13,10 +13,9 @@ int set_bit(unsigned long int *n, unsigned int index)
 
 	if (sizeof(n) != sizeof(unsigned long int))
 		return (-1);
-	
+
 	bit = 1 << index;
 	*n = *n | bit;
 
 	return (1);
-
 }
