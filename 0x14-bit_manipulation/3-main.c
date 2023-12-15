7dea5386acd1dfb3f3fd651a259cf0ea936468e8
FavourId <daeworfavour1@gmail.com>
2023-12-15 01:19:38 +0100

    done

--- 0x14-bit_manipulation/3-main.c
+++ 0x14-bit_manipulation/3-main.c
@@ -11,8 +11,8 @@ int main(void)
 	        n = 1024;
 		    set_bit(&n, 5);
 		        printf("%lu\n", n);
-			    n = 0;
-			        set_bit(&n, 'c');
+			    n = 2048;
+			        set_bit(&n, 2000);
 				    printf("%lu\n", n);
 				        n = 98;
 					    set_bit(&n, 0);
--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,25 +1,26 @@
 #include "main.h"
 /**
- * clear_bit: set bit to 0 at a given index
+ * clear_bit - set bit to 0 at a given index
  * @index: the index, starting from 0 of the bit you want to set
  * @n: poiter to the number
  *
+ * Return: return 1
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	int bit;
 	unsigned long zero = 0;
-	
+
 	if (*n < zero)
 	{
 		printf("Invalid index\n");
-		return -1;
+		return (-1);
 	}
 
 	bit = ~(1 << index);
 
 	*n = *n & bit;
 
-	return 1;
-		
+	return (1);
+
 }
