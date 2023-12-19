5eb82d806d714efabeba23bab7894c841bcd11d0
FavourId <daeworfavour1@gmail.com>
2023-12-19 03:52:10 +0100

    done

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,9 +8,9 @@
 #include <stdio.h>
 #include <sys/stat.h>
 
+#define BUFFER_SIZE 1024
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void print_hex(const unsigned char* buf, size_t len);
 
 #endif
