1b2758f97ef3ab9c33bc84224e46305a6651f578
FavourId <daeworfavour1@gmail.com>
2023-12-19 03:47:26 +0100

    done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,38 +1,37 @@
 #include "main.h"
 /**
- * create_file - create a new file
- *
- * @filename: name of the file to read
- * @letters: number of letters to read
- *
- * Return: number of chracters read
+ * create_file - creates a file
+ * @filename: the filename
+ * @text_content: the content to put in the file
+ * Return: Always 0.
  */
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	size_t wrtlen;
+	ssize_t written;
 
-	if (filename == NULL)
-		return (-1);
-
-	fd = open(filename,O_WRONLY | O_CREAT | O_TRUNC );
-	
-	if (fd == -1)
+	if (!filename)
 	{
 		return (-1);
 	}
-	
-	if (text_content != NULL)
-	{
-		wrtlen = write(fd, text_content, strlen(text_content));
 
-		if ((int)wrtlen == -1)
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+	if (text_content)
+	{
+		written = write(fd, text_content, strlen(text_content));
+		if (written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+		if ((size_t)written != strlen(text_content))
 		{
 			close(fd);
 			return (-1);
 		}
 	}
-
 	close(fd);
 	return (1);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,9 +6,11 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/stat.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void print_hex(const unsigned char* buf, size_t len);
 
 #endif
