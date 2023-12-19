622e20be2fa951637f77b5a09c946826dfbd2bee
FavourId <daeworfavour1@gmail.com>
2023-12-19 03:54:40 +0100

    done

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,24 +1,43 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include <stdlib.h>
-#include "main.h"
-
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * read_textfile - reads a text file and prints it to stdout.
+ * @filename: the filename
+ * @letters: number of letter to read and print
+ * Return: number of letters it could read and print
  */
-int main(int ac, char **av)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-    ssize_t n;
+	int fd;
+	ssize_t num_read, num_written;
+	char *buffer;
 
-    if (ac != 2)
-    {
-        dprintf(2, "Usage: %s filename\n", av[0]);
-        exit(1);
-    }
-    n = read_textfile(av[1], 18);
-    printf("\n(printed chars: %li)\n", n);
-    n = read_textfile(av[1], 1024);
-    printf("\n(printed chars: %li)\n", n);
-    return (0);
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buffer = malloc(letters * sizeof(char));
+
+	num_read = read(fd, buffer, letters);
+	if (num_read == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	num_written = write(STDOUT_FILENO, buffer, num_read);
+	if (num_written != num_read)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+	free(buffer);
+	close(fd);
+	return (num_read);
+
 }
