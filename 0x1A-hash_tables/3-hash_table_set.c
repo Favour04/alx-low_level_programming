9509e070263b35c08aefbcd86e99da394b698641
favour04 <idaeworfavour1@gmail.com>
2024-04-15 23:35:18 +0100

    added funtion to add element to table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,3 +1,45 @@
 #include "hash_tables.h"
-#include "tasks/3-hash_table_set.c"
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: hastable to add an element to
+ * @key: key of the element. NB (key can not be
+ * an empty string)
+ * @value: value associated with you
+ * Return: 1 if it succeeded, 0 otherwise
+ */
 
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	int indx;
+
+	if (key == NULL)
+		return (1);
+
+	indx = key_index((unsigned char *)key, ht->size);
+	ht->array[indx] = malloc(sizeof(hash_node_t));
+	ht->array[indx]->key = strdup(key);
+
+	if (ht->array[indx]->key != NULL)
+	{
+		while (ht->array[indx] != NULL)
+		{
+			ht->array[indx] = ht->array[indx]->next;
+		}
+
+		ht->array[indx] = malloc(sizeof(hash_node_t));
+		ht->array[indx]->key = strdup(key);
+		ht->array[indx]->value = strdup(value);
+		ht->array[indx]->next = NULL;
+
+		return (0);
+	}
+	else
+	{
+		ht->array[indx] = malloc(sizeof(hash_node_t));
+		ht->array[indx]->key = strdup(key);
+		ht->array[indx]->value = strdup(value);
+		ht->array[indx]->next = NULL;
+		return (0);
+	}
+	return (1);
+}
