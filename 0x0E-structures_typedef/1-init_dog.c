0a0514d277f8a0b88bc03e567d3e20770b4fd36f
favour04 <idaeworfavour1@gmail.com>
2024-06-15 00:37:49 +0100

    checkers added

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,13 +10,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
+	if (d != NULL)
 	{
-		d = NULL;
-		exit;
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
 	}
-
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
 }
